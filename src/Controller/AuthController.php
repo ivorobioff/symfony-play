f40cd01d03290923b293cfa7264fe842c1e668c4
Igor Vorobiov <igor.vorobioff@gmail.com>
2022-09-01 10:34:47 +0300

    some updates

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AuthController extends AbstractController
+{
+    #[Route('/login', name: 'auth_login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        return $this->render('auth/login.html.twig', [
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+            'last_username' => $authenticationUtils->getLastUsername()
+        ]);
+    }
+
+    #[Route('/logout', name: 'auth_logout', methods: ['GET'])]
+    public function logout(): Response {
+        throw new \RuntimeException('Don\'t forget to activate logout in security.yaml');
+    }
+}
